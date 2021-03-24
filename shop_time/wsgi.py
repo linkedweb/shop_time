d96e5b2e9e1f64b326289d59bab24f00fb555fae
Bryan Dunn <brkicb1@gmail.com>
2021-03-24 19:17:53 -0400

    Add whitenoise to wsgi

--- shop_time/wsgi.py
+++ shop_time/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_time.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
