6010bcf485014903d28d7601864ad56390d730e7
Bryan Dunn <brkicb1@gmail.com>
2021-03-24 18:48:18 -0400

    Changes for deployment

--- shop_time/wsgi.py
+++ shop_time/wsgi.py
@@ -11,9 +11,7 @@ import os
 from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_time.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
